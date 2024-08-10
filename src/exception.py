1efbfbcc5148c27bd5e5c65adbc9189591a7a1f4
Ravi <ravikumar6f@gmail.com>
2024-08-10 17:27:51 +0530

    added exception and logging file.

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,12 @@
 import sys
+import logging
+import logger
 
 def error_message_details(error,error_details:sys):
+    '''
+        error_details.exc_info() returns a tuple of three values: the type of the exception, the exception instance, and a traceback object.
+        _, _, exc_tb = error_details.exc_info(): Unpacks the traceback object (exc_tb).
+    '''
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message = f"Error occoured in script : {file_name}, line no : {exc_tb.tb_lineno} and error message is {str(error)}"
@@ -14,3 +20,16 @@ class CustomException(Exception):
 
     def __str__(self) -> str:
         return self.error_message
+    
+
+
+
+# testing exception.py
+if __name__== "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info(e)
+        raise CustomException(e,sys)
+        
+
